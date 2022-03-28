9d49b1a3795483c82ba3ac3f400e750c57af6c99
sanudevops <dhananjayapattnaik99@gmail.com>
2022-03-28 09:48:46 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import menu from './data'
+
+
+const unique=["all",...new Set(menu.map((cata)=>cata.category))]
 
 function App() {
+  const [item, setitem] = useState(menu);
+  const[catagory,setCatagory]=useState(unique)
+ function specific(category){
+    if(category==="all"){
+      return setitem(menu)
+    }
+    const updated=menu.filter((c)=>{
+      return c.category===category
+       })
+       setitem(updated)
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='header'>
+      <hi>our menu</hi>
+      <div className='catagories'>
+        {catagory.map((category)=>{
+          return(
+            <button onClick={()=>specific(category)}>{category}</button>
+          )
+        })}
+         
+        
+      </div>
     </div>
-  );
+    <div>
+     {item.map((item)=>{
+       console.log(item)
+       const {category,title}=item
+       return(
+         <>
+         <h1>{title}</h1>
+         <h1>{category}</h1>
+         </>
+       )
+     })}
+    </div>
+    </>
+  )
+
+
 }
 
 export default App;
